19ff88a245799f6d742059ac44c4a4d7eab9d3ee
Nikhil Sharma <95525120+niikkhilsharma@users.noreply.github.com>
2022-11-15 02:06:25 +0530

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,12 @@
+function greater(a,b){
+    if (a>b){
+        console.log(a +" is greater than B");
+    }
+    else {
+        console.log(b +" is greater than A");
+    }
+}
+let a =a.input();
+let b = b.input();
+// console.log("please enter the value of a "+ a.input())
+// console.log("please enter the value of b " + b.input())
